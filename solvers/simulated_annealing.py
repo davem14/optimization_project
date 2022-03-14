483a9580bf8c40c0af75f109e03985d86d8bd589
David Emanuel <david14em@gmail.com>
2022-03-14 17:30:41 +0200

    support mini-batch full-batch

--- solvers/simulated_annealing.py
+++ solvers/simulated_annealing.py
@@ -7,8 +7,8 @@ from solvers import Solver
 
 
 class SimulatedAnnealing(Solver):
-    def __init__(self, problem: Problem, init_state: tuple, cooling_factor: float = 0.97):
-        super().__init__(problem, init_state)
+    def __init__(self, problem: Problem, init_state: tuple, cooling_factor: float = 0.97, batch_size: int = 1):
+        super().__init__(problem, init_state, batch_size)
         self.iteration = 1
         self.temp = 1
         self.cooling_factor = cooling_factor
--- solvers/solver.py
+++ solvers/solver.py
@@ -6,8 +6,9 @@ from problems import Problem
 
 
 class Solver(ABC):
-    def __init__(self, problem: Problem, init_state: tuple):
+    def __init__(self, problem: Problem, init_state: tuple, batch_size: int = 1):
         self._problem = problem
+        self._batch_size = batch_size
         self._state = init_state
         self._values = [problem.evaluate(init_state)]
         self._running = True
@@ -17,7 +18,8 @@ class Solver(ABC):
         while self._running:
             changed = False
             neighbors = self._problem.neighbors(self._state)
-            for neighbor in neighbors:
+            for neighbors_batch in self.split_to_batches(neighbors):
+                neighbor = self.choose_best_from_batch(neighbors_batch)
                 neighbor_val = self._problem.evaluate(neighbor)
                 if self.is_preferred_state(neighbor, self._values[-1] - neighbor_val):
                     self._values.append(neighbor_val)
@@ -29,6 +31,19 @@ class Solver(ABC):
                     break
             if not changed:
                 return self._state
+
+    def split_to_batches(self, neighbors):
+        batch = []
+        for i, neighbor in enumerate(neighbors, 1):
+            batch.append(neighbor)
+            if i % self._batch_size == 0:
+                yield batch
+                batch = []
+        if batch:
+            yield batch
+
+    def choose_best_from_batch(self, neighbors):
+        return sorted(neighbors, key=lambda n: self._problem.evaluate(n))[0]
 
     def is_preferred_state(self, state, val) -> bool:
         raise NotImplementedError
